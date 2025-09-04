61654d6eee75d436e867d940ce534f5b603cecf2
Blacdav <davidaniefoik@gmail.com>
2025-09-04 15:13:13 +0100

    setup the layout for the dashboard

--- src/pages/dashboard/components/nav.tsx
+++ src/pages/dashboard/components/nav.tsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export const Sidebar: React.FC = () => {
   return (
-    <aside>
-      <img src="/assets/logo.webp" alt="" />
+    <aside className='col-span-2 hidden md:block border-r-2 border-neutral-100 h-screen px-5'>
+      <img src="/assets/logo.webp" alt="tbc-logo" className='w-1/2' />
+
+      <ul className='grid gap-8 mt-5'>
+        <li className='flex justify-between'>
+          <p className='flex gap-5 hover:cursor-pointer'>
+            <img src="/icons/home-outline.svg" alt="home" />
+            <span>Home</span>
+          </p>
+        </li>
+        <li className='flex gap-5 hover:cursor-pointer'>
+          <img src="/icons/search-outline.svg" alt="explore" />
+          <span>Explore</span>
+        </li>
+        <li className='flex gap-5 hover:cursor-pointer'>
+          <img src="/icons/people-outline.svg" alt="community" />
+          <span>Community</span>
+        </li>
+        <li className='flex gap-5 hover:cursor-pointer'>
+          <img src="/public/icons/user-outline.svg" alt="profile" />
+          <span>My Profile</span>
+        </li>
+        <li className='flex gap-5 hover:cursor-pointer'>
+          <img src="/public/icons/setting-outline.svg" alt="setting" />
+          <span>Settings</span>
+        </li>
+      </ul>
     </aside>
   )
 }
--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router"
+import { Sidebar } from "./components/nav"
+import Header from "./components/header"
 
 const DashboardLayout: React.FC = () => {
   return (
-    <main>
-      HOME
-      <section>
+    <main className="grid grid-cols-12">
+      <Sidebar />
+      <section className="col-span-10">
+        <Header />
         <Outlet />
       </section>
     </main>
