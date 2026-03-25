0f92a3af9ddebfc0a74a9b63f698b7c4470632b9
marvellrite <melodyozuru087@gmail.com>
2026-03-25 13:01:52 +0100

    Insert sufficient horizontal padding for the section element in homeLayout.tsx

--- src/app/home/components/layout/homeLayout.tsx
+++ src/app/home/components/layout/homeLayout.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from '@/lib/utils';
 // import '../globals.css';
 import { Tabs } from '../../components/nav';
 import { useSidebar } from '../../context/sidebar-context';
@@ -15,7 +16,7 @@ export default function HomeLayoutContent({
    return (
       <main className="flex flex-1 h-screen overflow-y-auto  w-full max-md:flex-col bg-surface-home">
          <Sidebar />
-         <section className={`@container grow overflow-y-auto flex-1 duration-600 transition-all ${isOpen ? 'md:ml-0' : 'md:ml-0'}`}>
+         <section className={cn('@container grow overflow-y-auto flex-1 duration-600 transition-all max-md:px-5', isOpen ? 'md:ml-0' : 'md:ml-0')}>
             {children}
          </section>
          <Tabs />
